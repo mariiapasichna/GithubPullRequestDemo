1bf3f8457f61f67a29e132ed1bd7a5f3c836d524
Mariia Pasichna <pasichnayam@gmail.com>
2020-03-16 15:13:59 +0200

    correct

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -26,17 +27,20 @@ public class Main {
                     .stream()
                     .filter(v -> v.contains("/"))
                     .filter(v -> v.contains("."))
-                    .map(v -> v.substring(0, v.indexOf("/")))
                     .map(v -> v.trim())
+                    .map(v -> v.substring(0, v.indexOf("/")))
                     .map(v -> v.replaceFirst("www.", ""))
                     .map(v -> v.replaceFirst("m.", ""))
                     .collect(Collectors.toList());
             Map<String, Long> map = streamFromFiles
                     .stream()
-                    .collect(Collectors.groupingBy(v -> v, Collectors.counting()));
-            List<Map.Entry<String, Long>> domains = new ArrayList<>(map.entrySet());
-            domains.sort((o1, o2) -> Long.compare(o2.getValue(), o1.getValue()));
-            System.out.println("Top 10 domains: " + domains.subList(0, 10));
+                    .collect(Collectors.groupingBy(v -> v, Collectors.counting()))
+                    .entrySet()
+                    .stream()
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                    .limit(10)
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+            System.out.println("Top 10 domains: " + map);
         } catch (IOException e) {
             System.out.println("File " + fileName + " not found");
         }
